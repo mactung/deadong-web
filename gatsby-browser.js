6aa3059c8ab819057f75c107ff52ef61f4c987a8
Mac Tung <mactrantung@gmail.com>
2020-08-10 21:07:34 +0700

    fix something make build slower

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,2 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./src/styles/index.scss"
-
-import CMS from "netlify-cms-app"
-import cloudinary from "netlify-cms-media-library-cloudinary"
-CMS.registerMediaLibrary(cloudinary)
